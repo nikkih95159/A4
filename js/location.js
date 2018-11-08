e7165d7433c95c2d789dfc15123829548c4c5f13
Nikki Hardiman <nhardima@ucsd.edu>
2018-11-07 16:11:08 -0800

    added bootstrap, radio functionality on time page, and location

--- js/location.js
+++ js/location.js
@@ -1,16 +1,7 @@
 // Call this function when the page loads (the jQuery "ready" event)
 $(document).ready(function() {
-	initializePage();
+	
 })
-
-/*
- * Function that is called when the document is ready.
- */
-function initializePage() {
-	$("#start").click(function(e) {
-		
-	});
-}
 
 var modal = document.getElementById('myModal');
 
@@ -36,3 +27,57 @@ window.onclick = function(event) {
 		modal.style.display = "none";
 	}
 }
+
+function geoFindMe() {
+	var output = document.getElementById("out");
+  
+	if (!navigator.geolocation){
+	  output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+	  return;
+	}
+  
+	function success(position) {
+	  var latitude  = position.coords.latitude;
+	  var longitude = position.coords.longitude;
+  
+	  output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+  
+	  var img = new Image();
+	  img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+  
+	  output.appendChild(img);
+	}
+  
+	function error() {
+	  output.innerHTML = "Unable to retrieve your location";
+	}
+  
+	output.innerHTML = "<p>Locating…</p>";
+  
+	navigator.geolocation.getCurrentPosition(success, error);
+	}
+
+	var radios = [].slice.call(document.querySelectorAll('input[name=gym]'));
+
+	function reset() {
+			document.getElementById('regencia').disabled = false;
+			document.getElementById('regencia').checked = false;
+			document.getElementById('costaverde').disabled = false;
+			document.getElementById('costaverde').checked = false;
+			document.getElementById('regents').disabled = false;
+			document.getElementById('regents').checked = false;
+			document.getElementById('rimac').disabled = false;
+			document.getElementById('rimac').checked = false;
+	}
+
+	radios.forEach(function(radio) {
+			radio.addEventListener('change', function() { 
+					radios.forEach(function(r) {
+							r.disabled = r !== radio;
+					});
+			});
+	});
+
+	function getLocation() {
+			var gymLocation = document.querySelector('input[name=gym]:checked').value;
+	}
