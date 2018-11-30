926d168e6548a440369275e1014391838cd63100
Nikki Hardiman <nhardima@ucsd.edu>
2018-11-30 00:35:28 -0800

    updated login & sign up features

--- js/home.js
+++ js/home.js
@@ -63,8 +63,10 @@ function store() {
 	 }
 
 	else{
-    localStorage.setItem('username', newid.value);
-    localStorage.setItem('userpw', newpass.value);
+    	localStorage.setItem('username', newid.value);
+    	localStorage.setItem('userpw', newpass.value);
+		// window.open('../A4/pages/homepage.html');
+		window.location.replace('../A4/pages/homepage.html');
 	}
 }
 
@@ -82,7 +84,8 @@ function check() {
 		//if loginid is equal to signupid in localStorage and logpass is equal to signuppass open next page
     if (logid==storedName && logpass==storedPw){
 			//window.open directs user to next page by opening up a new tab
-      window.open('../A4/pages/homepage.html');
+	//   window.open('../A4/pages/homepage.html');
+	window.location.replace('../A4/pages/homepage.html');
 			//window.location.href directs user to next page without opening a new tab
       //window.location.href = '../A4/pages/homepage.html';
     }
